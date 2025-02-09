331c898fa3cdc61bdaa35dc65978795d0fd5cd86
Norbert515 <kozsir.norbert@gmail.com>
2025-02-09 13:07:07 +0100

    bump to 0.0.2-alpha

--- Formula/vide.rb
+++ Formula/vide.rb
@@ -1,9 +1,9 @@
 class Vide < Formula
     desc "Vide"
     homepage "https://vide.dev"
-    version "0.0.1-alpha"
-    url "https://github.com/Norbert515/homebrew-tap/releases/download/v0.0.1-alpha/vide"
-    sha256 "d2e1feae1d9fe786ece516b2b88ba2545030616e010893b29f1f7ac2f489d4f5"
+    version "0.0.2-alpha"
+    url "https://github.com/Norbert515/homebrew-tap/releases/download/v0.0.2-alpha/vide"
+    sha256 "0f8ad8c1326dc128f7dc664db7994068e28240a2b9a2c9d05904425424a438c9"
     
     def install
       bin.install "vide"  # this installs the downloaded binary
@@ -13,6 +13,6 @@ class Vide < Formula
     test do
       # You can write a simple test to verify the binary was installed correctly.
       # For example, if the binary supports a '--version' flag:
-      assert_match version.to_s, shell_output("#{bin}/vide")
+      assert_match version.to_s, shell_output("#{bin}/vide --version")
     end
   end
