48ff0c8c3ffaeb2d28fd93f620d28b0f6efea2e6
Norbert515 <kozsir.norbert@gmail.com>
2025-02-12 18:11:20 +0100

    bump to 0.0.4-alpha

--- Formula/vide.rb
+++ Formula/vide.rb
@@ -1,9 +1,9 @@
 class Vide < Formula
     desc "Vide"
     homepage "https://vide.dev"
-    version "0.0.3-alpha"
-    url "https://github.com/Norbert515/homebrew-tap/releases/download/v0.0.3-alpha/vide"
-    sha256 "c844f247586ea3a7a694bf73e4ece5cf9e830dcfe5eeb03768479c5c3d620db7"
+    version "0.0.4-alpha"
+    url "https://github.com/Norbert515/homebrew-tap/releases/download/v0.0.4-alpha/vide"
+    sha256 "e1b911af81f1e2329a0932e1ec57b10a34c7e4092fc0bd8a494df3f2802491df"
     
     def install
       bin.install "vide"  # this installs the downloaded binary
