88b35b1c96fdc7afdc026c7f0d1b2a69843a6e2a
Norbert Kozsir <norbertkozsir@gmail.com>
2025-12-20 12:33:24 +0100

    vide 0.1.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/vide.rb
+++ Formula/vide.rb
@@ -1,9 +1,9 @@
 class Vide < Formula
     desc "Vide"
     homepage "https://vide.dev"
-    version "0.0.7-alpha"
-    url "https://github.com/Norbert515/homebrew-tap/releases/download/v0.0.7-alpha/vide_binary.zip"
-    sha256 "a6b4a2cedd47f6b3f5e74ccb261bd7533cc0bbefa8bfabf51b0e42e6bbefb85b"
+    version "0.1.0"
+    url "https://github.com/Norbert515/vide_cli/releases/download/v0.1.0/vide-macos"
+    sha256 "f94e8ebeb602c78c33540db875ba1d29cddef6025850c2a5fef48c88b782c575"
     
     def install
       # Unzip the downloaded file
