50eb903fa9bdeb1565241a2d71600a79fdb609dc
Norbert Kozsir <norbertkozsir@gmail.com>
2025-12-28 18:10:08 +0100

    vide 0.3.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/vide.rb
+++ Formula/vide.rb
@@ -1,9 +1,9 @@
 class Vide < Formula
   desc "An agentic terminal UI for Claude, built for Flutter developers"
   homepage "https://github.com/Norbert515/vide_cli"
-  version "0.2.0"
-  url "https://github.com/Norbert515/vide_cli/releases/download/v0.2.0/vide-macos.tar.gz"
-  sha256 "f1da39659328298cf1b70a21850002fd07fb1776fb7b9df326b829de1f9062d4"
+  version "0.3.0"
+  url "https://github.com/Norbert515/vide_cli/releases/download/v0.3.0/vide-macos.tar.gz"
+  sha256 "768de02bd1a45671c6e8c69b44cb4030163982ae65c811f0a904cfe61eb62c1f"
   license "Apache-2.0"
 
   def install
